d40f22228d7e64b511400323f1c9247670b7e047
Lennart Mühlhahn <lennart.muehlhahn@tuhh.de>
2026-03-11 23:38:42 +0100

    Added Publisher

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -38,6 +38,8 @@ public record Book(
         )
         Double price,
 
+        String publisher,
+
         @CreatedDate
         Instant createdDate,
 
@@ -48,9 +50,9 @@ public record Book(
         int version
 ) {
         @JsonCreator
-        public static Book of(String isbn, String title, String author, Double price) {
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
                 return new Book(
-                        null, isbn, title, author, price, null, null, 0
+                        null, isbn, title, author, price, publisher, null, null, 0
                 );
         }
 }
